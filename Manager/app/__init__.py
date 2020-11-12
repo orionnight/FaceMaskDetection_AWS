4a448eadb0ee2f2b38d4d2f52bdb955fde25d579
orionnight <abby.he@mail.utoronto.ca>
2020-11-12 21:53:06 +0800

    save template before plot

--- Manager/app/__init__.py
+++ Manager/app/__init__.py
@@ -6,12 +6,6 @@ import boto3
 app = Flask(__name__)
 app.config.from_object('app.settings')
 
-ec2 = boto3.resource('ec2', region_name='us-east-1')
-ec2_client = boto3.client('ec2', region_name='us-east-1')
-s3 = boto3.resource('s3', region_name='us-east-1')
-cw = boto3.client('cloudwatch', region_name='us-east-1')
-elb = boto3.client('elbv2', region_name='us-east-1')
-
 # Register all blueprints
 from .controllers import home
 app.register_blueprint(home.bp, url_prefix='/')
--- Manager/app/controllers/home.py
+++ Manager/app/controllers/home.py
@@ -2,8 +2,7 @@ from flask import Blueprint, render_template
 from flask import request, flash, redirect, url_for
 import boto3
 from datetime import timedelta, datetime
-
-from app import cw
+from operator import itemgetter
 
 
 bp = Blueprint('home', __name__, template_folder='../templates')
@@ -11,69 +10,83 @@ bp = Blueprint('home', __name__, template_folder='../templates')
 
 @bp.route('/', methods=['GET', 'POST'])
 def root():
-    
-    # instances = get_running_instances()
+    ec2 = boto3.resource('ec2')
+    instances = ec2.instances.all()
+    num_workers_stats = get_num_workers()
+    return render_template('home.html', num_workers_stats=num_workers_stats, instances=instances)
 
-    # number of workers for the past 30 minutes
 
-    # return render_template('home.html', instances=instances)
-    return render_template('home.html')
+@bp.route('/worker_info/<id>', methods=['GET', 'POST'])
+def worker_info(id):
+    cpu_stats = get_total_cpu_utilization(id)
+    http_stats = get_http_request_rate(id)
+    return render_template('worker_info.html', cpu_stats=cpu_stats, http_stats=http_stats)
+
+
+@bp.route('/worker_destroy/<id>', methods=['GET', 'POST'])
+def worker_destroy(id):
+    ec2 = boto3.resource('ec2')    
+    ec2.instances.filter(InstanceIds=[id]).terminate()
+    return redirect(url_for('home.root'))
 
 
 @bp.route('/manager', methods=['GET', 'POST'])
 def manager():
     
-    # instances = get_running_instances()
-
-    # number of workers for the past 30 minutes
-
-    # return render_template('home.html', instances=instances)
     return render_template('manager.html')
 
-def get_running_instances():
-    ec2 = boto3.resource('ec2')
-    instances = ec2.instances.filter(
-        Filters=[{'Name': 'instance-state-name', 'Values': ['running','pending']},
-                 {'Name': 'tag:Name', 'Values': ['worker']}])
-    return instances
 
 
 def get_num_workers():
-    workers = cw.get_metric_statistics(
+    client = boto3.client('cloudwatch')
+    workers = client.get_metric_statistics(
         Period = 60,
         StartTime = datetime.utcnow() - timedelta(seconds=30*60),
-        EndTime = datetime.utcnow(),
+        EndTime = datetime.utcnow() - timedelta(seconds=0 * 60),
         MetricName = 'NumWorkers',
         Namespace = 'AWS/EC2',
         Statistics = ['Average'],
-        Dimensions = [{'Name': 'InstanceId', 'Value': 'i-078f69c8c9c0097d6'}]
+        Dimensions = [{'Name': 'InstanceId', 'Value': 'i-0ef4896d3ab639d73'}]
     )
-
-    return return_label_values(workers, 'Average')
+    return processing_data(workers, 'Average')
 
 
 def get_total_cpu_utilization(id):
-    cpu = cw.get_metric_statistics(
-        Period = 60,
-        StartTime = datetime.utcnow() - timedelta(seconds=30*60),
-        EndTime = datetime.utcnow(),
-        MetricName = 'CPUUtilization',
-        Namespace = 'AWS/EC2',
-        Statistics = ['Average'],
-        Dimensions = [{'Name': 'InstanceId', 'Value': id}]
+    client = boto3.client('cloudwatch')
+    cpu = client.get_metric_statistics(
+        Period=1 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName='CPUUtilization',
+        Namespace='AWS/EC2',  # Unit='Percent',
+        Statistics=['Average'],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
     )
-    return return_label_values(cpu, 'Average')
+    return processing_data(cpu, 'Average')
 
 
 def get_http_request_rate(id):
-    http = cw.get_metric_statistics(
+    client = boto3.client('cloudwatch')
+    http = client.get_metric_statistics(
         Period = 60,
         StartTime = datetime.utcnow() - timedelta(seconds=30*60),
-        EndTime = datetime.utcnow(),
-        MetricName = 'HTTPRequestRate',
+        EndTime = datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName = 'HttpRequestCount',
         Namespace='AWS/EC2',
         Statistics = ['Sum'],
         Dimensions = [{'Name': 'InstanceId', 'Value': id}]
     )
+    return processing_data(http, 'Sum')
 
-    return return_label_values(http, 'Sum')
+
+def processing_data(metric, method):
+    data_stats = []
+
+    for point in metric['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute/60
+        data_stats.append([time, point[method]])
+
+    data_stats = sorted(metric, key=itemgetter(0))
+    return data_stats
